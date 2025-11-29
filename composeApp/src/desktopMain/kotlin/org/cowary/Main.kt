642e22ad53a07940019bece9893c4c519271d086
cowary <airvary7@gmail.com>
2025-11-29 20:27:09 +0300

    1.3.0

--- composeApp/src/desktopMain/kotlin/org/cowary/Main.kt
+++ composeApp/src/desktopMain/kotlin/org/cowary/Main.kt
@@ -4,11 +4,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPlacement
-import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.window.*
 import java.awt.Toolkit
 
 fun main() = application {
